acd09912c438f559c5889d0ecc3209c2038f9394
Athicha Popa <94356797+shadowbladk@users.noreply.github.com>
2023-11-15 16:28:38 +0700

    Change index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-import CourseCard from "@/components/CourseCard";
-
 export default function Page() {
-  return (
-    <div className="flex flex-row">
-      <CourseCard
-        imageSrc="python.svg"
-        courseTitle="Python Bootcamp"
-        instructorName="Marry Jane"
-      />
-    </div>
-  );
+  return <h1>Hello, Next.js!</h1>;
 }
