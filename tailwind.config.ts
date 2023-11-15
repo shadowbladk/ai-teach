574f1855feaae1036b27c0625165add2a632eb44
Athicha Popa <94356797+shadowbladk@users.noreply.github.com>
2023-11-15 19:30:27 +0700

    remove material tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
