f6e821b0ef17ad8fd8325be4e89bde542c1e080e
TawanL <tawancraft@outlook.com>
2024-01-01 06:47:26 +0700

    Add sign-in and sign-up pages and auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full flex items-center justify-items-center">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
