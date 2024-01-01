9a8bf543186c11941e7a78824caccbc1c6b47b18
TawanL <tawancraft@outlook.com>
2024-01-01 22:01:05 +0700

    Add Prisma database configuration and schema

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+export const db = globalThis.prisma || new PrismaClient();
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === "development") globalThis.prisma = db;
