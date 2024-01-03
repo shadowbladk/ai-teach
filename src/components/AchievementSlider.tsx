577b80d428a2dbf374f232b4412a1fc0f47637ca
faynch <83182929+faynch@users.noreply.github.com>
2024-01-03 22:45:33 +0700

    Achievement section

---
+++ src/components/AchievementSlider.tsx
@@ -0,0 +1,110 @@
+import React from "react"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore from "swiper"
+
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/free-mode"
+import "swiper/css/navigation"
+
+import { FreeMode, Pagination, Navigation } from "swiper/modules"
+
+import { Achievement } from "@/constants"
+
+import { useRef, useState } from "react"
+
+import Image from "next/image"
+
+import Left from "@/assets/left.svg"
+import LeftDisable from "@/assets/left-disable.svg"
+import Right from "@/assets/right.svg"
+import RightDisable from "@/assets/right-disable.svg"
+
+const Slider = () => {
+  const [canGoPrev, setCanGoPrev] = useState(false)
+  const [canGoNext, setCanGoNext] = useState(true)
+
+  const handleSlideChange = (swiper: any) => {
+    // Check if the swiper is at the beginning or end
+    const isBeginning = swiper.isBeginning
+    const isEnd = swiper.isEnd
+
+    // You can perform actions based on the current position
+
+    if (isBeginning) {
+      setCanGoPrev(false)
+    } else if (isEnd) {
+      setCanGoNext(false)
+    } else {
+      setCanGoPrev(true)
+      setCanGoNext(true)
+    }
+
+    console.log(`isBeginning: ${isBeginning}, ${canGoPrev}, isEnd: ${isEnd}`)
+  }
+
+  return (
+    <div className="flex flex-row gap-4 sm:gap-8 items-center justify-center">
+      <div className="custom-prev-button h-full min-w-[40px] sm:min-w-[60px]">
+        {canGoPrev ? (
+          <Image src={Left} alt={""} />
+        ) : (
+          <Image src={LeftDisable} alt={""} />
+        )}
+      </div>
+      <div className="flex flex-row justify-center items-center">
+        <Swiper
+          onSlideChange={(swiper) => handleSlideChange(swiper)}
+          slidesPerView={1}
+          breakpoints={{
+            1024: {
+              slidesPerView: 2,
+            },
+            1280: {
+              slidesPerView: 3,
+            },
+          }}
+          draggable={true}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={{
+            nextEl: ".custom-next-button",
+            prevEl: ".custom-prev-button",
+          }}
+          modules={[FreeMode, Navigation, Pagination]}
+          className="max-w-[300px] sm:max-w-sm lg:max-w-2xl xl:max-w-5xl"
+        >
+          {Achievement.map((item) => (
+            <SwiperSlide key={item.index} className="max-w-fit mb-8">
+              <div className="flex flex-col mx-20 gap-5 items-center group relative cursor-pointer">
+                <div className="w-[300px] h-[300px] p-4 flex flex-col gap-4 items-center">
+                  <Image
+                    className="object-none"
+                    src={item.image}
+                    alt={item.content}
+                  />
+
+                  <h3 className="text-xl text-center font-semibold">
+                    {item.content}
+                  </h3>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="custom-next-button h-full min-w-[40px] sm:min-w-[60px]">
+        {canGoNext ? (
+          <Image src={Right} alt={""} />
+        ) : (
+          <Image src={RightDisable} alt={""} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Slider
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer"
 
 import Card from "@/components/Card"
 
-import Slider from "@/components/Slider2"
+import Slider from "@/components/AchievementSlider"
 
 function profile() {
   const [toggle, setToggle] = useState(false)
@@ -41,7 +41,10 @@ function profile() {
               </div>
             </div>
           </section>
-          <section className="bg-white">
+          <section className="flex flex-col items-center justify-center px-24 py-16 bg-white gap-8">
+            <h1 className="text-[32px] font-extrabold text-center text-primary">
+            Achievement
+            </h1>
             <Slider />
           </section>
           <section className="flex flex-col justify-center py-12 px-12 sm:px-20 xl:px-28 gap-8">
@@ -70,8 +73,6 @@ function profile() {
               </button>
             </div>
             <div className="mb-8 grid grid-cols-1 gap-8 place-self-center lg:max-w-7xl lg:grid-cols-2 xl:grid-cols-3">
-              <Card />
-              <Card />
               <Card />
               <Card />
             </div>
