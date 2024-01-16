272055373f8aaeea91077c6f9bd2df1d62488104
TawanL <tawancraft@outlook.com>
2024-01-16 17:10:15 +0700

    Add CourseCard component

---
+++ components/course-card.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BookOpen } from "lucide-react";
+
+import { IconBadge } from "@/components/icon-badge";
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chartersLength: number;
+  progress: number | null;
+  category: string;
+}
+
+export const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chartersLength,
+  progress,
+  category,
+}: CourseCardProps) => {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image fill className="object-cover" alt={title} src={imageUrl} />
+        </div>
+        <div className="flex flex-col pt-2">
+          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">{category}</p>
+        </div>
+        <div>
+          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chartersLength} {chartersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
+            </div>
+          </div>
+          {progress !== null ? (
+            <div>TODO: Progress bar</div>
+          ) : (
+            <p className="text-md md:text-sm font-medium text-slate-700">
+              Not started
+            </p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
