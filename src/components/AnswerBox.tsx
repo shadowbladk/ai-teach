bedafca72f58e863f5acafc5e5c0e37353743bb1
Pro13574 <thanawich.joey@icloud.com>
2024-01-17 10:17:33 +0700

    Fix quiz question

--- src/components/AnswerBox.tsx
+++ src/components/AnswerBox.tsx
@@ -1,7 +1,7 @@
 export default function AnswerBox({answer}:{answer : string}) {
     return (
       <>
-          <div className="w-[430px] h-[60px] p-4 flex flex-col gap-4 border-4 rounded-xl bg-white w-full lg:flex-row items-center justify-center mx-5 my-2.5">
+          <div className="w-[432px] h-[60px] p-4 flex flex-col gap-4 border-4 rounded-xl bg-white w-full lg:flex-row items-center justify-center mx-5 my-2.5">
               <p className="text-[20px] font-normal text-left text-black">{answer}</p>
           </div>
       </>
--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,15 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
-import Image from "next/image"
-
-import HeroPic from "@/assets/hero.svg"
-import Levelup from "@/assets/levelup.svg"
-import Feedback from "@/assets/feedback.svg"
-import Quiz from "@/assets/quiz.svg"
-
-import Slider from "@/components/CourseSlider"
 import QuestionCard from "@/components/QuestionCard";
 import AnswerBox from "@/components/AnswerBox";
 
@@ -17,43 +8,40 @@ function quiz() {
     const [toggle, setToggle] = useState(false)
   
     return (
-      <>
       <div className="flex min-h-screen flex-col">
         <Navbar />
         <div className="flex-grow">
           <div className="container mx-auto justify-center max-w-[900px] ">
-          <section className="flex flex-col items-center justify-center px-23 py-16 gap-12">
-            <h1 className="text-[32px] font-extrabold text-center text-black">
-              Quiz
-            </h1>
-            <div className="flex flex-col w-full max-w-[900px] lg:flex-row items-center justify-between">
-              <p className="text-[24px] font-extrabold text-left text-black">
-                Quiz 1 - Review Chapter 1
-              </p>
-              <p className="text-[24px] font-extrabold text-right text-black">
-                Question 1/10
-              </p>
-            </div>
-            <div className="flex flex-col w-full justify-center">
-              <QuestionCard>
-              </QuestionCard>
-            </div>
-            <div className="flex flex-col">
-            <div className="flex flex-col w-full lg:flex-row items-center justify-between">
-              <AnswerBox answer="Defining user-defined functions"></AnswerBox>
-              <AnswerBox answer="Naming variables and objects"></AnswerBox>
-            </div>
-            <div className="flex flex-col w-full lg:flex-row items-center justify-between">
-              <AnswerBox answer="Identifying syntax errors in code"></AnswerBox>
-              <AnswerBox answer="Signifying reserved words"></AnswerBox>
-            </div>
-            </div>
-          </section>
+            <section className="flex flex-col items-center justify-center px-23 py-16 gap-12">
+              <h1 className="text-[32px] font-extrabold text-center text-black">
+                Quiz
+              </h1>
+              <div className="flex flex-col w-full max-w-[900px] lg:flex-row items-center justify-between">
+                <p className="text-[24px] font-extrabold text-left text-black">
+                  Quiz 1 - Review Chapter 1
+                </p>
+                <p className="text-[24px] font-extrabold text-right text-black">
+                  Question 1/10
+                </p>
+              </div>
+              <div className="flex flex-col w-full justify-center">
+                <QuestionCard/>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex flex-col w-full lg:flex-row items-center justify-between">
+                  <AnswerBox answer="Defining user-defined functions"></AnswerBox>
+                  <AnswerBox answer="Naming variables and objects"></AnswerBox>
+                </div>
+                <div className="flex flex-col w-full lg:flex-row items-center justify-between">
+                  <AnswerBox answer="Identifying syntax errors in code"></AnswerBox>
+                  <AnswerBox answer="Signifying reserved words"></AnswerBox>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
         <Footer />
       </div>
-      </>
     )
   }
   
