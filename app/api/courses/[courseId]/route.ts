d5f19ea32a9a3d3c38fbbe5f26188150beeeeaf4
Tawan Lekngam <tawancraft@outlook.com>
2024-02-08 08:33:20 +0700

    Add route for chapter

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -4,10 +4,10 @@ import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
-// const { Video } = new Mux(
-//   process.env.MUX_TOKEN_ID!,
-//   process.env.MUX_TOKEN_SECRET!,
-// );
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!
+);
 
 export async function DELETE(
   req: Request,
@@ -29,20 +29,20 @@ export async function DELETE(
         chapters: {
           include: {
             muxData: true,
-          }
-        }
-      }
+          },
+        },
+      },
     });
 
     if (!course) {
       return new NextResponse("Not found", { status: 404 });
     }
 
-    // for (const chapter of course.chapters) {
-    //   if (chapter.muxData?.assetId) {
-    //     await Video.Assets.del(chapter.muxData.assetId);
-    //   }
-    // }
+    for (const chapter of course.chapters) {
+      if (chapter.muxData?.assetId) {
+        await Video.Assets.del(chapter.muxData.assetId);
+      }
+    }
 
     const deletedCourse = await db.course.delete({
       where: {
@@ -73,11 +73,11 @@ export async function PATCH(
     const course = await db.course.update({
       where: {
         id: courseId,
-        userId
+        userId,
       },
       data: {
         ...values,
-      }
+      },
     });
 
     return NextResponse.json(course);
