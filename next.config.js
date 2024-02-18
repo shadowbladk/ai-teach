64febd14d7f136bf2d8da7ac9e6c8f7c9cefe11a
Athicha Popa <94356797+shadowbladk@users.noreply.github.com>
2024-02-18 22:17:04 +0700

    Exclude sharp and canvas from webpack processing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
+  webpack: (config) => {
+    // Exclude sharp and canvas from being processed by webpack
+    config.externals.push({
+      sharp: 'commonjs sharp',
+      canvas: 'commonjs canvas'
+    });
+    return config;
+  }
 };
 
 module.exports = nextConfig;
