74710107d6ab012f9d09774b84159f3814fd2a79
Pro13574 <thanawich.joey@icloud.com>
2024-02-22 03:17:24 +0700

    adjust flipicon

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -1,3 +1,28 @@
+// const WordCard: React.FC<WordCardProps> = ({
+//   word,
+//   definition,
+//   isFlipped,
+//   setIsFlipped,
+// }) => {
+//   return (
+//     <div className="justify-center flex flex-col border-4 border-purple-500/100 rounded-xl bg-white p-4">
+//       <div className="p-4">
+//         <p className="text-[20px] font-normal text-center text-black items-center">
+//           {isFlipped ? definition : word}
+//         </p>
+//       </div>
+//       <button
+//         className="mt-4 grid justify-items-end"
+//         onClick={() => setIsFlipped(!isFlipped)}
+//       >
+//         <Image src={Flipicon} alt="-" width={50} height={50} />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default WordCard;
+
 import Flipicon from "@/assets/flipicon.svg";
 import Image from "next/image";
 import React from "react";
@@ -5,8 +30,8 @@ import React from "react";
 interface WordCardProps {
   word: string;
   definition: string;
-  isFlipped: boolean; // Add this
-  setIsFlipped: (isFlipped: boolean) => void; // Add this
+  isFlipped: boolean;
+  setIsFlipped: (isFlipped: boolean) => void;
 }
 
 const WordCard: React.FC<WordCardProps> = ({
@@ -16,17 +41,19 @@ const WordCard: React.FC<WordCardProps> = ({
   setIsFlipped,
 }) => {
   return (
-    <div className="justify-center flex flex-col border-4 border-purple-500/100 rounded-xl bg-white p-4">
-      <div className="p-4">
-        <p className="text-[20px] font-normal text-center text-black items-center">
-          {isFlipped ? definition : word}
-        </p>
+    <div className="relative flex justify-center items-start"> {/* Container with relative position */}
+      <div className="flex flex-col border-4 border-purple-500/100 rounded-xl bg-white p-4"> {/* Word box */}
+        <div className="p-4">
+          <p className="text-[20px] font-normal text-center text-black items-center">
+            {isFlipped ? definition : word}
+          </p>
+        </div>
       </div>
       <button
-        className="mt-4 grid justify-items-end"
+        className="absolute top-0 right-0 mt-4 mr-4 transform p-4 translate-x-full" // Position the button outside the box
         onClick={() => setIsFlipped(!isFlipped)}
       >
-        <Image src={Flipicon} alt="-" width={50} height={50} />
+        <Image src={Flipicon} alt="Flip" width={50} height={50} />
       </button>
     </div>
   );
