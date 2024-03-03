0b3361c266bfe021bd0b9b75bfec6b8e5cc4be13
TawanL <tawancraft@outlook.com>
2024-03-03 18:56:09 +0700

    Add delete method to route.ts

--- app/api/courses/[courseId]/chapters/[chapterId]/(attachments)/flashcarddeck/[flashcarddeckId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/(attachments)/flashcarddeck/[flashcarddeckId]/route.ts
@@ -4,27 +4,92 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
 export async function POST(
-  reg: Request,
-  { params }: { params: { courseId: string; chapterId: string } }
+  req: Request,
+  params: {
+    courseId: string;
+    chapterId: string;
+    flashcarddeckId: string;
+  }
+) {
+  try {
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  params: {
+    courseId: string;
+    chapterId: string;
+    flashcarddeckId: string;
+  }
 ) {
   try {
     const { userId } = auth();
-    const { title } = await reg.json();
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const flashcarddeck = await db.flashcardDeck.create({
-      data: {
-        chapterId: params.chapterId,
-        title,
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
       },
     });
 
-    return NextResponse.json(flashcarddeck);
+    if (!course) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const flashcardDeck = await db.flashcardDeck.findUnique({
+      where: {
+        id: params.flashcarddeckId,
+      },
+    });
+
+    if (!flashcardDeck) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
   } catch (error) {
-    console.log("[FLASHCARDDECK]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  params: {
+    courseId: string;
+    flashcarddeckId: string;
+  }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const deleteFlashcardDeck = await db.flashcardDeck.delete({
+      where: {
+        id: params.flashcarddeckId,
+      },
+    });
+
+    return NextResponse.json(deleteFlashcardDeck);
+  } catch (error) {
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
