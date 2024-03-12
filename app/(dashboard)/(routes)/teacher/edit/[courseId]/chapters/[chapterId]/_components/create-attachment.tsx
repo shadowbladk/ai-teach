13f60cde6e0f1edf646abf3c3decc44175ce6371
Athicha Popa <94356797+shadowbladk@users.noreply.github.com>
2024-03-12 23:08:07 +0700

    Add new icon for flashcard and quiz

--- app/(dashboard)/(routes)/teacher/edit/[courseId]/chapters/[chapterId]/_components/create-attachment.tsx
+++ app/(dashboard)/(routes)/teacher/edit/[courseId]/chapters/[chapterId]/_components/create-attachment.tsx
@@ -1,72 +1,98 @@
-import { Card, CardContent } from "@/components/ui/card"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { FileText, PlusCircle, Video } from "lucide-react"
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  ClipboardList,
+  FileText,
+  PlusCircle,
+  Video,
+  WalletCards,
+} from "lucide-react";
+import Link from "next/link";
 
 export const CreateAttachment = () => {
-    return (
-        <Dialog>
-            <DialogTrigger>
-                <PlusCircle size={40} strokeWidth={1.5} />
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle className="text-center font-bold text-xl">
-                        Choose Type of Content
-                    </DialogTitle>
-                    <DialogDescription>
-                        <div className="pt-5 grid grid-cols-2 gap-2 justify-items-center">
-                            <div className="text-center">
-                                <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
-                                    <CardContent className="flex items-center justify-center p-2" >
-                                        <FileText size={40} strokeWidth={1.5}/>
-                                    </CardContent>
-                                </Card>
-                                <h3 className="pt-2 text-base">PDF Files</h3>
-                            </div>
-                            <div className="text-center">
-                                <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
-                                    <CardContent className="flex items-center justify-center p-2" >
-                                        <Video size={40} strokeWidth={1.5}/>
-                                    </CardContent>
-                                </Card>
-                                <h3 className="pt-2 text-base">Videos</h3>
-                            </div>
-                            <div className="text-center">
-                                <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
-                                    <CardContent className="flex items-center justify-center p-2" >
-                                        <FileText size={40} strokeWidth={1.5}/>
-                                    </CardContent>
-                                </Card>
-                                <h3 className="pt-2 text-base">PDF Files</h3>
-                            </div>
-                            <div className="text-center">
-                                <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
-                                    <CardContent className="flex items-center justify-center p-2" >
-                                        <FileText size={40} strokeWidth={1.5}/>
-                                    </CardContent>
-                                </Card>
-                                <h3 className="pt-2 text-base">PDF Files</h3>
-                            </div>
-                            <div className="text-center">
-                                <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
-                                    <CardContent className="flex items-center justify-center p-2" >
-                                        <FileText size={40} strokeWidth={1.5}/>
-                                    </CardContent>
-                                </Card>
-                                <h3 className="pt-2 text-base">PDF Files</h3>
-                            </div>
-                            <div className="text-center">
-                                <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
-                                    <CardContent className="flex items-center justify-center p-2" >
-                                        <FileText size={40} strokeWidth={1.5}/>
-                                    </CardContent>
-                                </Card>
-                                <h3 className="pt-2 text-base">PDF Files</h3>
-                            </div>
-                        </div>
-                    </DialogDescription>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>)
-    
-}
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <PlusCircle size={40} strokeWidth={1.5} />
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-center font-bold text-xl">
+            Choose Type of Content
+          </DialogTitle>
+          <DialogDescription>
+            <div className="pt-5 grid grid-cols-2 gap-2 justify-items-center">
+              <div className="text-center">
+                <Link href="">
+                  <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <FileText size={40} strokeWidth={1.5} />
+                    </CardContent>
+                  </Card>
+                </Link>
+                <h3 className="pt-2 text-base">PDF Files</h3>
+              </div>
+              <div className="text-center">
+                <Link href="">
+                  <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <Video size={40} strokeWidth={1.5} />
+                    </CardContent>
+                  </Card>
+                </Link>
+                <h3 className="pt-2 text-base">Videos</h3>
+              </div>
+              <div className="text-center">
+                <Link href="">
+                  <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <ClipboardList size={40} strokeWidth={1.5} />
+                    </CardContent>
+                  </Card>
+                </Link>
+                <h3 className="pt-2 text-base">Quizzes</h3>
+              </div>
+              <div className="text-center">
+                <Link href="">
+                  <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <WalletCards size={40} strokeWidth={1.5} />
+                    </CardContent>
+                  </Card>
+                </Link>
+                <h3 className="pt-2 text-base">Flashcards</h3>
+              </div>
+              <div className="text-center">
+                <Link href="">
+                  <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <FileText size={40} strokeWidth={1.5} />
+                    </CardContent>
+                  </Card>
+                </Link>
+                <h3 className="pt-2 text-base">PDF Files</h3>
+              </div>
+              <div className="text-center">
+                <Link href="">
+                  <Card className="w-[60px] h-[60px] rounded-full bg-[#D9D9D9]">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <FileText size={40} strokeWidth={1.5} />
+                    </CardContent>
+                  </Card>
+                </Link>
+                <h3 className="pt-2 text-base">PDF Files</h3>
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
