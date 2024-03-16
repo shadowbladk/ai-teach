6355d898f1fe241b71d337684ba59c5b2d7f3c3c
TawanL <tawancraft@outlook.com>
2024-03-16 20:37:43 +0700

    Add document creation route

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/(attachments)/documents/route.ts
@@ -0,0 +1,24 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function POST(
+  reg: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    //   TODO: Implement document creation
+
+    return NextResponse.json({});
+  } catch (error) {
+    console.log("[DOCUMENT", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
