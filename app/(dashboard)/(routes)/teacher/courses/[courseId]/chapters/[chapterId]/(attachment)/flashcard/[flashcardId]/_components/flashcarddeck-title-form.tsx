1dbf839c4945d28d89cd2dc75140b7fd741e61cd
faynch <83182929+faynch@users.noreply.github.com>
2024-03-16 14:27:44 +0700

    fix edit title

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/(attachment)/flashcard/[flashcardId]/_components/flashcarddeck-title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/(attachment)/flashcard/[flashcardId]/_components/flashcarddeck-title-form.tsx
@@ -1,14 +1,14 @@
-"use client";
+"use client"
 
-import axios from "axios";
-import toast from "react-hot-toast";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+import axios from "axios"
+import toast from "react-hot-toast"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
 
-import { Pencil } from "lucide-react";
+import { Pencil } from "lucide-react"
 
 import {
   Form,
@@ -16,22 +16,22 @@ import {
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 interface FlashcarddeckTitleFormProps {
   initialData: {
-    title: string;
-  };
-  courseId: string;
-  chapterId: string;
-  flashcarddeckId: string;
+    title: string
+  }
+  courseId: string
+  chapterId: string
+  flashcarddeckId: string
 }
 
 const formSchema = z.object({
   title: z.string().min(1),
-});
+})
 
 export const FlashcarddeckTitleForm = ({
   initialData,
@@ -39,50 +39,54 @@ export const FlashcarddeckTitleForm = ({
   chapterId,
   flashcarddeckId,
 }: FlashcarddeckTitleFormProps) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const toggleEdit = () => setIsEditing((current) => !current);
+  const [isEditing, setIsEditing] = useState(false)
+  const toggleEdit = () => setIsEditing((current) => !current)
 
-  const router = useRouter();
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
-  });
+  })
 
-  const { isSubmitting, isValid } = form.formState;
+  const { isSubmitting, isValid } = form.formState
 
   const onSubmit = async (value: z.infer<typeof formSchema>) => {
     try {
       await axios.patch(
         `/api/courses/${courseId}/chapters/${chapterId}/flashcarddecks/${flashcarddeckId}`,
         value
-      );
-      toast.success("Flashcard deck updated");
+      )
+      toast.success("Flashcard deck updated")
       toggleEdit()
-      router.refresh();
+      router.refresh()
     } catch {
-      toast.error("Something went wrong");
+      toast.error("Something went wrong")
     }
-  };
+  }
 
   return (
     <div className="border bg-slate-100 rounded-md p-6 flex flex-col gap-4">
-      <div className="font-medium flex justify-between">
-        Flash card deck title
-        <Button
-          onClick={toggleEdit}
-          variant="underline"
-          size="ghost"
-          className={isEditing ? "hidden" : "flex"}
-        >
-          <Pencil className="h-4 w-4 mr-1" />
-          Edit title
-        </Button>
-      </div>
-      {!isEditing && <p className="text-sm">{initialData.title}</p>}
+      {!isEditing && (
+        <div className="font-medium flex justify-between">
+          {initialData.title}
+          <Button
+            onClick={toggleEdit}
+            variant="underline"
+            size="ghost"
+            className={isEditing ? "hidden" : "flex"}
+          >
+            <Pencil className="h-4 w-4 mr-1" />
+            Edit title
+          </Button>
+        </div>
+      )}
       {isEditing && (
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 pt-2"
+          >
             <FormField
               control={form.control}
               name="title"
@@ -121,5 +125,5 @@ export const FlashcarddeckTitleForm = ({
         </Form>
       )}
     </div>
-  );
-};
+  )
+}
