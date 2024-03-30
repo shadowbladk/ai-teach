01d2132d74bd7b3b05620b40587f8c8a133ff80f
TawanL <tawancraft@outlook.com>
2024-03-30 11:38:19 +0700

    Add question route handlers for DELETE and PATCH methods

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/(attachments)/questionset/[questionsetId]/questions/[questionId]/route.ts
@@ -0,0 +1,81 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function DELETE(
+  req: Request,
+  {
+    params,
+  }: { params: { courseId: string; chapterId: string; questionId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const deleteQuiz = await db.question.delete({
+      where: {
+        id: params.questionId,
+      },
+    });
+
+    return NextResponse.json(deleteQuiz);
+  } catch (error) {
+    console.log("[QUESTION_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: { courseId: string; chapterId: string; questionId: string };
+  }
+) {
+  try {
+    const { userId } = auth();
+    const values = await req.json();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const quiz = await db.question.update({
+      where: {
+        id: params.questionId,
+      },
+      data: {
+        ...values,
+      },
+    });
+    return NextResponse.json(quiz);
+  } catch (error) {
+    console.log("[QUESTION_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
