666892be2654951c1979326d4f8385f3ab80c11f
Athicha Popa <94356797+shadowbladk@users.noreply.github.com>
2024-04-05 12:35:56 +0700

    Connect quiz to backend

---
+++ app/courses/[courseId]/chapters/[chapterId]/(attachment)/quiz/[quizId]/page.tsx
@@ -0,0 +1,50 @@
+import { db } from "@/lib/db";
+import Quiz from "./_components/Quiz";
+import { redirect } from "next/navigation";
+
+const quizPage = async ({ params }: { params: { quizId: string } }) => {
+  const quiz = await db.questionSet.findUnique({
+    where: {
+      id: params.quizId,
+    },
+    include: {
+      Question: {
+        orderBy: {
+          createdAt: "asc",
+        },
+        include: {
+          answers: {
+            orderBy: {
+              createdAt: "asc",
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!quiz) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex-grow">
+        <div className="container mx-auto justify-center max-w-[900px] ">
+          <Quiz
+            title={quiz.title}
+            questions={quiz.Question.map((question) => ({
+              id: question.id,
+              question: question.text || "", // Assuming text can be null
+              choices: question.answers.map((answer) => answer.text || ""),
+              answer:
+                question.answers.find((answer) => answer.isCorrect)?.text || "",
+            }))}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default quizPage;
